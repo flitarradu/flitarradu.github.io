baf6061ac4284b4cfa03e4b9adc35ed964919f61
Flitar Radu <flitar.radu@gmail.com>
2019-04-14 16:06:35 +0300

    Added react project

---
+++ build/precache-manifest.ab1fcac94f698ad2b7d949725fa66e87.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd49408e35d159105d69",
+    "url": "/static/css/main.f6b41010.chunk.css"
+  },
+  {
+    "revision": "fd49408e35d159105d69",
+    "url": "/static/js/main.baa02619.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b4a26a6c68292a81cc6c",
+    "url": "/static/css/2.773672e0.chunk.css"
+  },
+  {
+    "revision": "b4a26a6c68292a81cc6c",
+    "url": "/static/js/2.17961c60.chunk.js"
+  },
+  {
+    "revision": "effd78a6318469a093bead5306b69e87",
+    "url": "/static/media/Logo.effd78a6.png"
+  },
+  {
+    "revision": "ac7d59404104b5a4c6a4cde2a99a2671",
+    "url": "/static/media/banner.ac7d5940.jpg"
+  },
+  {
+    "revision": "96b4bfe903b0ee93e9cdf24747918bda",
+    "url": "/static/media/cover1.96b4bfe9.jpg"
+  },
+  {
+    "revision": "63f28fbe3b7f3cc1cb2b8549120d6bcc",
+    "url": "/static/media/cover2.63f28fbe.jpg"
+  },
+  {
+    "revision": "f8ee6ceee17da508b1dc4ade73826cdc",
+    "url": "/static/media/cover3.f8ee6cee.jpg"
+  },
+  {
+    "revision": "6e61c344f918455d2368a0c1b8400fa8",
+    "url": "/static/media/testim1.6e61c344.jpg"
+  },
+  {
+    "revision": "929025ab9fc9e4e8fe3f7b597e5e9f32",
+    "url": "/static/media/testim2.929025ab.jpg"
+  },
+  {
+    "revision": "07f0024dbe54493e56cc97fa40a3a825",
+    "url": "/static/media/man1.07f0024d.png"
+  },
+  {
+    "revision": "b5b4f438796b9cf30cd7ce2877fa9b70",
+    "url": "/static/media/star1.b5b4f438.png"
+  },
+  {
+    "revision": "37db37fb8e35e0cca4d18acd83a6a11d",
+    "url": "/static/media/star2.37db37fb.png"
+  },
+  {
+    "revision": "d641109f46a9231b7a92d6a114302a2b",
+    "url": "/static/media/Raleway-Black.d641109f.ttf"
+  },
+  {
+    "revision": "178e018a8aa8992181f7b0e393c6a0cc",
+    "url": "/index.html"
+  }
+];
